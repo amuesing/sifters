53992587bf3c5253e1cad23755c937978d750aee
muesinga <contact@aaronmuesing.com>
2022-08-15 23:04:14 -0700

    transposition

--- sonic-pi-projects/clavier/transposition.rb
+++ sonic-pi-projects/clavier/transposition.rb
@@ -1,6 +1,20 @@
 row = [60, 64, 67, 72, 76]
 
-def transposition(row)
 
+# given an array of integers generate a new array off of 
+# every index which maintains the same interval structure.
+
+def transposition(r)
+    i = 1
+    while i < r.length
+        while
+        x = r[0] - r[y]
+    end
 end
-    
+
+# expecting
+# [60, 64, 67, 72, 76],
+# [56, 60, 63, 68, 72],
+# [53, 57, 60, 65, 69],
+# [48, 52, 55, 60, 64],
+# [44, 48, 51, 56, 60]
