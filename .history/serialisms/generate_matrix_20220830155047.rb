729ddc8ed48d8222102848e2fb8c079044b02017
muesinga <contact@aaronmuesing.com>
2022-08-30 22:45:10 -0700

    multi-file structure

---
+++ .history/serialisms/generate_matrix_20220830155047.rb
@@ -0,0 +1,13 @@
+def generate_matrix(row)
+    x = []
+    y = []
+    z = []
+    row.each do |n|
+        x << (n - row.first)
+        y << Array.new(row.length) {row.first + (row.first - n)}
+    end
+    y.each do |n| 
+        z << n.zip(x).rowap(&:sum)
+    end
+    row.replace(z)
+end
