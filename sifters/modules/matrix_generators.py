f06569d409af44c1fc5906a6fd18dbc58f7f833e
muesinga <contact@aaronmuesing.com>
2022-10-02 17:35:32 -0700

    transition matrix

--- sifters/modules/matrix_generators.py
+++ sifters/modules/matrix_generators.py
@@ -36,8 +36,9 @@ def fibonacci(fund, length):
                 i, note = note, i + note
     return [list(array) for array in np.array_split(np.array(matrix), len(seq))]
 
-def overtone(fund, length):
+def overtones(fund, length):
     # what if fund is 0 -- return error message
+    # use enunerate for partial instead of for loop
     partial = 1
     overtones = []
     subtones = []
@@ -56,7 +57,33 @@ def overtone(fund, length):
             partial += 1
     return [list(array) for array in np.array_split(np.array(matrix), len(overtones))]
 
+#the following code takes a list such as
+#[1,1,2,6,8,5,5,7,8,8,1,1,4,5,5,0,0,0,1,1,4,4,5,1,3,3,4,5,4,1,1]
+#with states labeled as successive integers starting with 0
+#and returns a transition matrix, M,
+#where M[i][j] is the probability of transitioning from i to j
 
+def transition_matrix(transitions):
+    n = 1+ max(transitions) #number of states
+
+    M = [[0]*n for _ in range(n)]
+
+    for (i,j) in zip(transitions,transitions[1:]):
+        M[i][j] += 1
+
+    #now convert to probabilities:
+    for row in M:
+        s = sum(row)
+        if s > 0:
+            row[:] = [f/s for f in row]
+    return M
+
+# https://stackoverflow.com/questions/46657221/generating-markov-transition-matrix-in-python
+
+def main(arg=None):
+    t = [ 0,0,1,0,2,0,3]
+    m = transition_matrix(t)
+    for row in m: print(' '.join('{0:.2f}'.format(x) for x in row))
 
 if __name__ == '__main__':
-    print(overtone(220, 5))
+    main()
