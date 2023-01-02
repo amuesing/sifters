a9b8bc28c715137e3576f8b5a6b32ff167bfe89e
muesinga <contact@aaronmuesing.com>
2023-01-01 19:10:43 -0800

    Composition class

---
+++ sifters/composition.py
@@ -0,0 +1,143 @@
+import music21
+import numpy
+import itertools
+import pandas
+
+class Composition:
+    def __init__(self, sivs):
+        self.intervals = self.get_intervals(sivs)
+        self.bin = self.get_binary(sivs)
+        self.period = len(self.bin[0])
+        self.factors = self.get_factors(self.period)
+        self.voices = len(self.factors)
+        self.grid = 1
+        
+    def get_binary(self, sivs):
+        bin = []
+        if isinstance(sivs, tuple):
+            per = []
+            obj = []
+            for siv in sivs:
+                objects = music21.sieve.Sieve(siv)
+                obj.append(objects)
+                per.append(objects.period())
+            lcm = numpy.lcm.reduce(per)
+            for o in obj:
+                o.setZRange(0, lcm - 1)
+                bin.append(o.segment(segmentFormat='binary'))
+        else:
+            obj = music21.sieve.Sieve(sivs)
+            obj.setZRange(0, obj.period() - 1)
+            bin.append(obj.segment(segmentFormat='binary'))
+        return bin
+    
+    def get_intervals(self, sivs):
+        intervals = []
+        for siv in sivs:
+            set = music21.sieve.Sieve(siv)
+            set.setZRange(0, set.period() - 1)
+            intervals.append(set.segment())
+        return intervals
+    
+    def get_factors(self, num):
+        factors = []
+        i = 1
+        while i <= num:
+            if num % i == 0:
+                factors.append(i)
+            i += 1
+        return factors
+    
+    def get_largest_prime_factor(self, n):
+        for i in range(n, 1, -1):
+            if n % i == 0 and all(i % j for j in range(2, i)):
+                return i
+        return 1
+    
+    def construct_score(self, part):
+            df = self.generate_dataframe(part)
+            form = self.csv_to_midi(df, self.grid)
+            comp = self.set_measure_zero(form)
+            comp.write('midi', 'sifters/tracks/percussion.mid')
+            
+    def generate_dataframe(self, score):
+        parts = score.parts
+        rows_list = []
+        for part in parts:
+            for elt in part.getElementsByClass([music21.note.Note]):
+                d = {'Offset': elt.offset}
+                if hasattr(elt, 'pitches'):
+                    d.update({'Midi': pitch.midi for pitch in elt.pitches})
+                rows_list.append(d)
+        return pandas.DataFrame(rows_list).drop_duplicates()
+    
+    def csv_to_midi(self, df, grid):
+        part = music21.stream.Score()
+        result = {}
+        for _, row in df.iterrows():
+            offset = row['Offset']
+            mid = int(row['Midi'])
+            result[offset] = result.get(offset, []) + [mid]
+        for offset, mid in result.items():
+            notes = [music21.note.Note(m, quarterLength=grid) for m in mid]
+            part.insert(offset, music21.chord.Chord(notes) if len(notes) > 1 else notes[0])
+        return part.makeRests(fillGaps=True)
+    
+    def set_measure_zero(self, score):
+        score.insert(0, music21.metadata.Metadata())
+        score.metadata.title = 'Sifters'
+        score.metadata.composer = 'Ari Müsing'
+        score.insert(0, music21.instrument.UnpitchedPercussion())
+        score.insert(0, music21.clef.PercussionClef())
+        score.insert(0, music21.meter.TimeSignature('5/4'))
+        score.insert(0, music21.tempo.MetronomeMark('fast', 144, music21.note.Note(type='quarter')))
+        return score
+
+class Percussion(Composition):
+    def __init__(self, composition):
+        self.composition = composition
+        self.voices = composition.voices
+        self.bin = composition.bin
+        self.factors = composition.factors
+        self.grid = composition.grid
+        self.period = composition.period
+        self.part = music21.stream.Score()
+        
+    def create_parts(self):
+        parts = []
+        for i in range(self.voices):
+            p = music21.stream.Stream()
+            for b in self.bin:
+                p.insert(0, self.create_notes(b, self.factors[i]))
+            for notes in p:
+                parts.insert(0, notes)
+        for p in parts:
+            self.part.insert(0, p)
+            
+    def create_notes(self, bin, factor):
+        part = music21.stream.Part()
+        repeat = 1
+        pattern = (bin * factor) * repeat
+        dur = self.grid * (self.period / factor)
+        events = self.midi_pool(bin)
+        midi_pool = itertools.cycle(events)
+        for i, bit in enumerate(pattern):
+            if bit == 1:
+                part.insert(i * dur, music21.note.Note(midi=next(midi_pool), quarterLength=self.grid))
+        return part
+    
+    def midi_pool(self, bin):
+        events = bin.count(1)
+        lpf_slice = slice(0, self.get_largest_prime_factor(events))
+        instrument_pool = itertools.cycle([60,61,62,63,64][lpf_slice])
+        return [next(instrument_pool) for _ in range(events)]
+
+    def construct_score(self):
+        Composition.construct_score(self, self.part)
+
+if __name__ == '__main__':
+    sivs = '((8@0|8@1|8@7)&(5@1|5@3))', '((8@0|8@1|8@2)&5@0)', '((8@5|8@6)&(5@2|5@3|5@4))', '(8@6&5@1)', '(8@3)', '(8@4)', '(8@1&5@2)'
+    comp = Composition(sivs)
+    perc = Percussion(comp)
+    perc.create_parts()
+    perc.construct_score()
