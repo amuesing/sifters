c18d0741b9920a5f51d885bde24e86350b32733c
Aaron Muesing <a.muesing@gmail.com>
2023-12-01 22:41:05 -0800

    updated matrix method

--- sifters/texture.py
+++ sifters/texture.py
@@ -47,19 +47,46 @@ class Texture:
         return [interval * decimal.Decimal(str(i)) for i in range(period)]
     
     
+    # def generate_pitchclass_matrix(self, intervals):
+    #     # Calculate the interval between each pair of consecutive pitches.
+    #     next_interval = intervals[1:]
+    #     row = [decimal.Decimal('0.0')] + [next_interval[i] - intervals[0] for i, _ in enumerate(intervals[:-1])]
+
+    #     # Normalize the tone row so that it starts with 0 and has no negative values.
+    #     row = [note % 12 for note in row]
+
+    #     # Generate the rows of the pitch class matrix.
+    #     matrix = [[(abs(note - 12) % 12)] for note in row]
+
+    #     # Generate the columns of the pitch class matrix.
+    #     matrix = [row * len(intervals) for row in matrix]
+
+    #     # Update the matrix with the correct pitch class values.
+    #     matrix = [[(matrix[i][j] + row[j]) % 12
+    #             for j, _ in enumerate(range(len(row)))]
+    #             for i in range(len(row))]
+
+    #     # Label the rows and columns of the matrix.
+    #     matrix = pandas.DataFrame(matrix,
+    #                             index=[f'P{m[0]}' for m in matrix], 
+    #                             columns=[f'I{i}' for i in matrix[0]])
+        
+    #     return matrix
+    
+    
     def generate_pitchclass_matrix(self, intervals):
         next_interval = intervals[1:]
         row = [next_interval[i] - intervals[0] for i in range(len(intervals) - 1)]
-
+        
         matrix = [
             [
-                (intervals[0] - row[i]) % (self.period - 1)
+                (0 - row[i]) % (self.period - 1)
             ] 
             for i in range(len(intervals) - 1)
         ]
 
         row.insert(0, 0)
-        matrix.insert(0, [intervals[0]])
+        matrix.insert(0, [0])
 
         matrix = [
             [
@@ -109,7 +136,7 @@ class Texture:
         notes_data = []
 
         for factor_index in range(len(self.factors)):
-            tonality = decimal.Decimal(40.0)
+            tonality = 40
             steps = self.get_successive_diff(self.indices)
             steps_cycle = itertools.cycle(steps)
             first_pitch = tonality + self.indices[0]
@@ -117,14 +144,11 @@ class Texture:
             segment = self.segment_octave_by_period(self.period)
             intervals = [segment[i] for i in self.indices]
             matrix = self.generate_pitchclass_matrix(self.indices)
-            
             print(matrix)
-            
-            # matrix = first_pitch + self.generate_pitchclass_matrix(intervals)
-        #     num_of_events = (len(self.intervals) * self.factors[factor_index])
-        #     num_of_positions = num_of_events // len(steps)
-        #     pool = self.generate_note_pool_from_matrix(matrix, num_of_positions, steps_cycle)
-        #     flattened_pool = [num for list in pool for num in list]
+            # num_of_events = (len(self.intervals) * self.factors[factor_index])
+            # num_of_positions = num_of_events // len(steps)
+            # pool = self.generate_note_pool_from_matrix(matrix, num_of_positions, steps_cycle)
+            # flattened_pool = [num for list in pool for num in list]
 
         #     note_pool = itertools.cycle(flattened_pool)
         #     pattern = numpy.tile(self.binary, self.factors[factor_index])
@@ -135,5 +159,4 @@ class Texture:
         #         velocity = 64
         #         start = index * duration
         #         notes_data.append((self.texture_id, start, velocity, next(note_pool), duration))
-
         # return notes_data
