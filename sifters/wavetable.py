d224fa4ae5ebc54910b0339b4bd631ebfbee7193
Aaron Muesing <a.muesing@gmail.com>
2023-12-22 14:33:39 -0800

    approach to wavetable additive synthesis in main method

--- sifters/wavetable.py
+++ sifters/wavetable.py
@@ -7,19 +7,17 @@ from matplotlib import gridspec
 class Wavetable:
     
     def __init__(self, mediator):
-        # self.grids_set = mediator.grids_set
-        # self.binary = mediator.binary
-        # self.selected_cents_implied_zero = mediator.texture.selected_cents_implied_zero
-        # self.frequency_list = self.cents_to_frequency(self.reference_frequency, self.selected_cents_implied_zero)
-
         self.num_samples = 2048
         self.sample_rate = 96000
         self.reference_frequency = 46.875
-        duration = 1.0
         self.time = numpy.arange(0, self.num_samples) / self.sample_rate
         
-        self.sine_wave = self.generate_sine_wave
-    
+        # self.sine_wave = self.generate_sine_wave()
+        # self.grids_set = mediator.grids_set
+        # self.binary = mediator.binary
+        # self.selected_cents_implied_zero = mediator.texture.selected_cents_implied_zero
+        # self.frequency_list = [self.reference_frequency] + self.cents_to_frequency(self.reference_frequency, self.selected_cents_implied_zero)
+        
     
     def visualize_waveforms(self, waveforms, titles=None, combined_title='Waveform Visualization'):
         num_waveforms = len(waveforms)
@@ -31,17 +29,19 @@ class Wavetable:
 
         plt.figure(figsize=(8, 6))
 
-        for i, waveform in enumerate(waveforms, 1):
-            ax = plt.subplot(gs[i - 1])
+        for i, waveform in enumerate(waveforms):
+            ax = plt.subplot(gs[i])
             ax.plot(self.time, waveform)
-            ax.set_title(titles[i - 1] if titles else f'Waveform {i}')
+            ax.set_title(titles[i] if titles else f'Waveform {i + 1}')
             ax.set_xlabel('Time (s)')
             ax.set_ylabel('Amplitude')
             ax.grid(True)
+            ax.set_ylim([-1.1 * max(abs(waveform)), 1.1 * max(abs(waveform))])  # Adjust y-axis range
 
         plt.suptitle(combined_title, y=1.02)
         plt.tight_layout()
         plt.show()
+
         
         
     def cents_to_frequency(self, reference_frequency, cents_list):
@@ -57,29 +57,39 @@ class Wavetable:
     #     sine_wave = numpy.sin(2 * numpy.pi * self.reference_frequency * self.time)
     #     return self.normalize_waveform(sine_wave)
     
-    def generate_sine_wave_equal_parts(self, num_parts=1):
-        phase_offsets = 2 * numpy.pi * numpy.arange(num_parts) / num_parts
-        sine_wave = numpy.sin(2 * numpy.pi * self.reference_frequency * self.time + phase_offsets[:, numpy.newaxis])
-        return self.normalize_waveform(numpy.sum(sine_wave, axis=0))
+    # def generate_sine_wave(self, amplitude=.3, frequency=None, time=None, phase=0):
+    #     frequency = frequency if frequency is not None else self.reference_frequency
+    #     time = time if time is not None else self.time
+
+    #     sine_wave = amplitude * numpy.sin(2 * numpy.pi * frequency * time + phase)
+    #     return self.normalize_waveform(sine_wave)
     
-    def generate_sine_wave(self, phase_offset=0):
-        sine_wave = numpy.sin(2 * numpy.pi * self.reference_frequency * self.time + phase_offset)
-        return self.normalize_waveform(sine_wave)
+    # def generate_sine_wave(self, phase_offset=0):
+    #     sine_wave = numpy.sin(2 * numpy.pi * self.reference_frequency * self.time + phase_offset)
+    #     return self.normalize_waveform(sine_wave)
+    
+    
+    # def generate_sawtooth_wave(self):
+    #     sawtooth_wave = numpy.mod(self.time * self.reference_frequency, 1)
+    #     return self.normalize_waveform(sawtooth_wave)
 
 
-    def generate_sawtooth_wave(self):
-        sawtooth_wave = numpy.mod(self.time * self.reference_frequency, 1)
-        return self.normalize_waveform(sawtooth_wave)
+    # def generate_square_wave(self):
+    #     square_wave = numpy.sign(numpy.sin(2 * numpy.pi * self.frequency * self.time))
+    #     return self.normalize_waveform(square_wave)
 
 
-    def generate_square_wave(self):
-        square_wave = numpy.sign(numpy.sin(2 * numpy.pi * self.frequency * self.time))
-        return self.normalize_waveform(square_wave)
+    # def generate_triangle_wave(self):
+    #     triangle_wave = 2 * numpy.abs(numpy.mod(self.time * self.frequency, 1) - 0.5) - 0.5
+    #     return self.normalize_waveform(triangle_wave)
+    
+    
+    def generate_sine_wave(self, amplitude=1, frequency=None, time=None, phase=0):
+        frequency = frequency if frequency is not None else self.reference_frequency
+        time = time if time is not None else self.time
 
-
-    def generate_triangle_wave(self):
-        triangle_wave = 2 * numpy.abs(numpy.mod(self.time * self.frequency, 1) - 0.5) - 0.5
-        return self.normalize_waveform(triangle_wave)
+        sine_wave = amplitude * numpy.sin(2 * numpy.pi * frequency * time + phase)
+        return sine_wave
 
 
     def perform_linear_fm_synthesis(self, carrier_signal, modulating_signal, modulation_index=0.1):
@@ -90,23 +100,42 @@ class Wavetable:
     def perform_exponential_fm_synthesis(self, carrier_signal, modulating_signal, modulation_index=0.01):
         modulated_signal = carrier_signal * numpy.sin(2 * numpy.pi * numpy.exp(modulation_index * modulating_signal * self.time))
         return self.normalize_waveform(modulated_signal)
+    
+    
+    def perform_additive_synthesis(self, frequencies, amplitudes):
+        additive_wave = numpy.zeros(self.num_samples)
+        for frequency, amplitude in zip(frequencies, amplitudes):
+            harmonic_wave = self.generate_sine_wave(amplitude, frequency)
+            additive_wave += harmonic_wave
+        return self.normalize_waveform(additive_wave)
 
 
 if __name__ == '__main__':
-    # Write methods for various modulation methods that combine multiple modulation frequencies such as Cascading and Parallel techniques
     wave = Wavetable(mediator=None)
-    
-    # sine_wave = wave.generate_sine_wave()
-    # write("data/wav/sine_wave.wav", wave.sample_rate, sine_wave)
-    # wave.visualize_waveforms([sine_wave])
-    
-    # # Example binary sequence
-    # binary_sequence = [0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0]
-    # changes = [1, 1, 6, 1, 7, 1, 6, 1, 7, 1, 1, 1, 6]
-    
-    # sine_wave = wave.generate_sine_wave(phase_offset=numpy.pi/2)
-    sine_wave = wave.generate_sine_wave_equal_parts(2)
 
-    write("data/wav/sine_wave.wav", wave.sample_rate, sine_wave)
-    wave.visualize_waveforms([sine_wave])
+    frequencies = [46.875, 55.74, 59.75, 60.79, 71.05, 72.29, 77.48]
+    amplitudes = [1, .1, .1, .1, .1, .1, .1]
 
+    # Generate individual sine waves
+    individual_waves = [wave.generate_sine_wave(amplitude, frequency) for amplitude, frequency in zip(amplitudes, frequencies)]
+
+    # Perform and save additive synthesis step by step
+    for i in range(len(frequencies)):
+        partial_sum_waveform = wave.perform_additive_synthesis(frequencies[:i+1], amplitudes[:i+1])
+        normalized_partial_sum_waveform = wave.normalize_waveform(partial_sum_waveform)
+
+        # Save the partial sum waveform as a WAV file
+        write(f'partial_sum_step_{i+1}.wav', wave.sample_rate, normalized_partial_sum_waveform)
+
+    # Visualize the final additive synthesis waveform
+    additive_waveform = wave.perform_additive_synthesis(frequencies, amplitudes)
+    normalized_additive_waveform = wave.normalize_waveform(additive_waveform)
+
+    # Save the final additive synthesis waveform as a WAV file
+    write('final_additive_synthesis.wav', wave.sample_rate, normalized_additive_waveform)
+
+    # Visualize individual sine waves and the final additive synthesis waveform on the same graph
+    all_waves = individual_waves + [normalized_additive_waveform]
+    wave_titles = [f'Sine Wave {i+1}' for i in range(len(individual_waves))] + ['Final Additive Synthesis']
+
+    wave.visualize_waveforms(all_waves, titles=wave_titles, combined_title='Individual Sine Waves and Final Additive Synthesis')
