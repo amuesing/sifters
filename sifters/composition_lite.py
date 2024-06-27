fc8b5e65ec53fb0e855826621bea5236bac5d94f
Arial Muesing <morgiana@Morgiana.local>
2024-06-27 13:04:57 -0700

    velocity approach implemented

--- sifters/composition_lite.py
+++ sifters/composition_lite.py
@@ -4,125 +4,151 @@ import numpy
 
 title = 'starbird'
 
-# Rhythmic patterns dictionary
-sieve_dict = {
-    'snare': '(8@0|8@1|8@7)&(5@1|5@3)|((8@0|8@1|8@2)&5@0)|((8@5|8@6)&(5@2|5@3|5@4))',
-    'clap': '(((8@0|8@1|8@7)&(5@1|5@3))|((8@0|8@1|8@2)&5@0)|((8@5|8@6)&(5@2|5@3|5@4))|(8@6&5@1)|(8@3)|(8@4)|(8@1&5@2))',
-    'kick': '(8@3)|(8@4)',
-    'woodblock': '(8@1&5@2)',
-    'impact': '(8@6&5@1)'
+instrument_dict = {
+    'snare': {
+        'sieve': '(8@0|8@1|8@7)&(5@1|5@3)|((8@0|8@1|8@2)&5@0)|((8@5|8@6)&(5@2|5@3|5@4))',
+        'velocity_profile': {'gap': 120, 'primary': 100, 'secondary': 80, 'overlap': 60},
+        'accent_dict': {
+            'primary': '5@0|5@2|5@1|5@3',
+            'secondary': '8@0|8@1|8@2|8@5|8@6|8@7'
+        }
+    },
+    'clap': {
+        'sieve': '(((8@0|8@1|8@7)&(5@1|5@3))|((8@0|8@1|8@2)&5@0)|((8@5|8@6)&(5@2|5@3|5@4))|(8@6&5@1)|(8@3)|(8@4)|(8@1&5@2))',
+        'velocity_profile': {'gap': 120, 'primary': 100, 'secondary': 80, 'overlap': 60},
+        'accent_dict': {
+            'primary': '5@0|5@1|5@2|5@3|5@4',
+            'secondary': '8@0|8@1|8@2|8@3|8@4|8@5|8@6|8@7'
+        }
+    },
+    'kick': {
+        'sieve': '(8@3)|(8@4)',
+        'velocity_profile': {'gap': 120, 'primary': 100, 'secondary': 80, 'overlap': 60},
+        'accent_dict': {
+            'primary': '(8@3)',
+            'secondary': '(8@4)'
+        }
+    },
+    'woodblock': {
+        'sieve': '(8@1&5@2)',
+        'velocity_profile': {'gap': 120, 'primary': 100, 'secondary': 80, 'overlap': 60},
+        'accent_dict': {
+            'primary': '5@2',
+            'secondary': '8@1'
+        }
+    },
+    'impact': {
+        'sieve': '(8@6&5@1)',
+        'velocity_profile': {'gap': 120, 'primary': 100, 'secondary': 80, 'overlap': 60},
+        'accent_dict': {
+            'primary': '5@1',
+            'secondary': '8@6',
+        }
+    }
 }
 
-def prime_sieve(sieve):
-    binary = numpy.array(sieve.segment(segmentFormat='binary'))
-    return binary
+def sieve_to_binary(sieve):
+    return numpy.array(sieve.segment(segmentFormat='binary'))
 
-# Shift transformation
-def shift_sieve(sieve, shift_amount):
-    binary = numpy.array(sieve.segment(segmentFormat='binary'))  # Convert to NumPy array
-    shifted_binary = numpy.roll(binary, shift_amount)
-    return shifted_binary
+def transform_sieve(sieve, transform, *args):
+    binary = sieve_to_binary(sieve)
+    transformed_binary = transform(binary, *args)
+    return transformed_binary
 
-# Inversion transformation
-def invert_sieve(sieve):
-    binary = numpy.array(sieve.segment(segmentFormat='binary'))  # Convert to NumPy array
-    inverted_binary = 1 - binary
-    return inverted_binary
+def shift_binary(binary, shift_amount):
+    return numpy.roll(binary, shift_amount)
 
-# Reverse transformation
-def reverse_sieve(sieve):
-    binary = numpy.array(sieve.segment(segmentFormat='binary'))  # Convert to NumPy array
-    reversed_binary = binary[::-1]
-    return reversed_binary
+def invert_binary(binary):
+    return 1 - binary
 
-# Stretching (Scaling) transformation
-def stretch_sieve(sieve, factor):
-    binary = numpy.array(sieve.segment(segmentFormat='binary'))  # Convert to NumPy array
-    stretched_binary = numpy.repeat(binary, factor)
-    return stretched_binary
+def reverse_binary(binary):
+    return binary[::-1]
 
-def create_sieve_objs(sieve_dict):
+def stretch_binary(binary, factor):
+    return numpy.repeat(binary, factor)
 
-    sieve_objs = []
-    
-    for name, sieve in sieve_dict.items():
-        s = music21.sieve.Sieve(sieve)
-        sieve_objs.append((name, s))
+def create_sieve_objs(instrument_dict):
+    return [(name, music21.sieve.Sieve(info['sieve'])) for name, info in instrument_dict.items()]
 
-    return sieve_objs
+def find_largest_period(instrument_dict):
+    return max(music21.sieve.Sieve(info['sieve']).period() for info in instrument_dict.values())
 
-def find_largest_period(sieve_dict):
-    periods = []
+def create_accent_binaries(accent_dict, largest_period):
+    accent_binaries = {}
+    for name, sieve_pattern in accent_dict.items():
+        sieve_obj = music21.sieve.Sieve(sieve_pattern)
+        sieve_obj.setZRange(0, largest_period - 1)  # Set the zRange to largest_period
+        accent_binaries[name] = sieve_to_binary(sieve_obj)
+    return accent_binaries
 
-    for _, sieve in sieve_dict.items():
-        s = music21.sieve.Sieve(sieve)
-        period = s.period()
-        periods.append(period)
-
-    largest_period = max(periods)
-
-    return largest_period
-
-def create_midi(binary, period, filename):
-    # Create a MIDI file
+def create_midi(binary, period, filename, velocities):
     mid = mido.MidiFile()
-
-    # Create a MIDI track
     track = mido.MidiTrack()
     mid.tracks.append(track)
 
     ticks_per_quarter_note = 480
-    # Duration should match with denominator
     duration = ticks_per_quarter_note // 4
-    
-    # Add note events
-    for value in binary:
-        if value == 0:
-            track.append(mido.Message('note_on', note=64, velocity=0, time=0))
-            track.append(mido.Message('note_off', note=64, velocity=0, time=duration))
-        else:
-            track.append(mido.Message('note_on', note=64, velocity=100, time=0))
-            track.append(mido.Message('note_off', note=64, velocity=100, time=duration))
 
-    # Create a time signature message
-    numerator = period
-    denominator = 16  # You can adjust this as needed
-    time_signature = mido.MetaMessage('time_signature', numerator=numerator, denominator=denominator, time=0)
+    for value, velocity in zip(binary, velocities):
+        track.append(mido.Message('note_on', note=64, velocity=0 if value == 0 else velocity, time=0))
+        track.append(mido.Message('note_off', note=64, velocity=0 if value == 0 else velocity, time=duration))
+
+    time_signature = mido.MetaMessage('time_signature', numerator=period, denominator=16, time=0)
     track.append(mido.MetaMessage('track_name'))
-
     track.append(time_signature)
-    # Save the MIDI file
     mid.save(f'data/mid/{filename}')
 
-sieve_objs = create_sieve_objs(sieve_dict)
-# print(sieve_objs)
-largest_period = find_largest_period(sieve_dict)
+def accent_velocity_with_patterns(binary, primary_binary, secondary_binary, velocity_profile):
+    velocities = []
 
-for n, s in sieve_objs:
-    s.setZRange(0, largest_period - 1)
-    binary = s.segment(segmentFormat='binary')
+    primary_length = len(primary_binary)
+    secondary_length = len(secondary_binary)
+
+    for i, value in enumerate(binary):
+        if value == 0:
+            velocities.append(0)
+        elif primary_binary[i % primary_length] == 1 and secondary_binary[i % secondary_length] == 1:
+            velocities.append(velocity_profile['overlap'])
+        elif primary_binary[i % primary_length] == 1:
+            velocities.append(velocity_profile['primary'])
+        elif secondary_binary[i % secondary_length] == 1:
+            velocities.append(velocity_profile['secondary'])
+        else:
+            velocities.append(velocity_profile['gap'])
+
+    return velocities
+
+def process_sieve(s, name, period, accent_binaries, velocity_profile):
+    transformations = [
+        ('prime', lambda x: x),
+        ('invert', invert_binary),
+        ('reverse', reverse_binary),
+        ('stretch_2', lambda x: stretch_binary(x, 2))
+    ]
+
+    primary_binary = accent_binaries['primary']
+    secondary_binary = accent_binaries['secondary']
+
+    for suffix, transform in transformations:
+        transformed_sieve = transform_sieve(s, transform)
+        filename = f'{title}_{name}_{suffix}.mid'
+        velocities = accent_velocity_with_patterns(transformed_sieve, primary_binary, secondary_binary, velocity_profile)
+        create_midi(transformed_sieve, period, filename, velocities)
+
+    binary = sieve_to_binary(s)
     indices = numpy.nonzero(binary)[0]
-    
-    # Apply transformations and create MIDI files
-    for i in indices:
-        filename = f'{title}_{n}_shift_clip{i + 1}.mid'
-        shifted_sieve = shift_sieve(s, i)
-        create_midi(shifted_sieve, largest_period, filename)
-    
-    original_sieve = prime_sieve(s)
-    filename = f'{title}_{n}_prime.mid'
-    create_midi(original_sieve, largest_period, filename)
 
-    inverted_sieve = invert_sieve(s)
-    filename = f'{title}_{n}_invert.mid'
-    create_midi(inverted_sieve, largest_period, filename)
-    
-    reversed_sieve = reverse_sieve(s)
-    filename = f'{title}_{n}_reverse.mid'
-    create_midi(reversed_sieve, largest_period, filename)
-    
-    # Example of stretching with a factor of 2
-    f = 2
-    stretched_sieve = stretch_sieve(s, f)
-    filename = f'{title}_{n}_stretch_{f}.mid'
-    create_midi(stretched_sieve, largest_period, filename)
+    for i in indices:
+        filename = f'{title}_{name}_shift_clip{i + 1}.mid'
+        shifted_sieve = transform_sieve(s, shift_binary, i)
+        velocities = accent_velocity_with_patterns(shifted_sieve, primary_binary, secondary_binary, velocity_profile)
+        create_midi(shifted_sieve, period, filename, velocities)
+
+# Main processing loop
+sieve_objs = create_sieve_objs(instrument_dict)
+largest_period = find_largest_period(instrument_dict)
+
+for name, s in sieve_objs:
+    s.setZRange(0, largest_period - 1)
+    accent_binaries = create_accent_binaries(instrument_dict[name]['accent_dict'], largest_period)
+    process_sieve(s, name, largest_period, accent_binaries, instrument_dict[name]['velocity_profile'])
